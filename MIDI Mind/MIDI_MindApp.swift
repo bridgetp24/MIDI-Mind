74697654ebf3945140f8ce9eb9d23d22fedaa620
bridgetp24 <bridgetpeterkin@gmail.com>
2022-03-08 12:57:27 -0600

    Initial Commit

---
+++ MIDI Mind/MIDI_MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDI_MindApp.swift
+//  MIDI Mind
+//
+//  Created by Bridget Peterkin on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MIDI_MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

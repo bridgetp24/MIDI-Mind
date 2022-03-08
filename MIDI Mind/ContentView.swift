e5c9cdc65bd10cda40d87c60579bfd1529cf2e89
bridgetp24 <bridgetpeterkin@gmail.com>
2022-03-08 13:37:58 -0600

    First commit

--- MIDI Mind/ContentView.swift
+++ MIDI Mind/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+        Text("MIDI Mind")
             .padding()
+            .font(.system(size:60))
+            Spacer()
+            HStack(spacing:20.0) {
+            Button(action: {
+                print("try to play music")
+            }, label: {
+                Image(systemName: "play.circle.fill")
+            })
+            Button(action: {
+                print("pause music")
+            }, label: {
+                Image(systemName: "pause.circle.fill")
+            })
+            
+        }.font(.system(size: 60))
+            Spacer()
+            Image("brain")
+                
+                
+        }
+        
     }
 }
 
